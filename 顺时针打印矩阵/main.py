5e931d9c39d4695906195a04e7b4518412f2de66
zuiyee <zuiyeechina@gmail.com>
2019-02-22 23:24:01 +0800

    Create main.py

---
+++ 顺时针打印矩阵/main.py
@@ -0,0 +1,27 @@
+class Solution:
+
+    def printMatrix(self, matrix):
+        res = []
+        while matrix:
+            res += matrix.pop(0)
+            if matrix and matrix[0]:
+                for row in matrix:
+                    res.append(row.pop())
+            if matrix:
+                res += matrix.pop()[::-1]
+            if matrix and matrix[0]:
+                for row in matrix[::-1]:
+                    res.append(row.pop(0))
+        print(res)
+        return res
+
+matrix = [[1,  2,  3,  4],
+          [5,  6,  7,  8],
+          [9, 10, 11, 12],
+          [13, 14, 15, 16]]
+matrix2 = [[1],[2],[3],[4],[5]]
+matrix3 = [[1,2],[3,4],[5,6],[7,8],[9,10]]
+S = Solution()
+S.printMatrix(matrix)
+S.printMatrix(matrix2)
+S.printMatrix(matrix3)
