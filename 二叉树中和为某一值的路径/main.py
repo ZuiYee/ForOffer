1ddfc29290f51b8e8d6e4ed396451422a2d2b26f
zuiyee <zuiyeechina@gmail.com>
2019-02-25 22:12:25 +0800

    Update main.py

--- 二叉树中和为某一值的路径/main.py
+++ 二叉树中和为某一值的路径/main.py
@@ -6,29 +6,52 @@ class TreeNode:
         self.right = None
 
 
+# class Solution:
+#     # 返回二维列表，内部每个列表表示找到的路径
+#
+#     def Find(self, root, expectNumber, lis, lisAll):
+#         lis.append(root.val)
+#         expectNumber -= root.val
+#         if expectNumber == 0 and not root.left and not root.right:
+#             lisAll.append(lis)
+#             print(11, lisAll)
+#         if root.left:
+#             self.Find(root.left, expectNumber, lis, lisAll)
+#         if root.right:
+#             self.Find(root.right, expectNumber, lis, lisAll)
+#         lis.pop()
+#         return lisAll
+#
+#     def FindPath(self, root, expectNumber):
+#         lis = []
+#         listAll = []
+#         if not root:
+#             return listAll
+#         return self.Find(root, expectNumber, lis, listAll)
+import copy
 class Solution:
     # 返回二维列表，内部每个列表表示找到的路径
-
-    def Find(self, root, expectNumber, lis, lisAll):
-        lis.append(root.val)
-        expectNumber -= root.val
-        if expectNumber == 0 and not root.left and not root.right:
-            lisAll.append(lis)
-            print(11, lisAll)
+    def Find(self, root, num, path, lis):
+        path.append(root.val)
+        num -= root.val
+        if num == 0 and not root.left and not root.right:
+            lis.append(copy.deepcopy(path))
         if root.left:
-            self.Find(root.left, expectNumber, lis, lisAll)
+            lis = self.Find(root.left, num, path, lis)
         if root.right:
-            self.Find(root.right, expectNumber, lis, lisAll)
-        lis.pop()
-        return lisAll
+            lis = self.Find(root.right, num, path, lis)
+        path.pop()
+        return lis
 
     def FindPath(self, root, expectNumber):
-        lis = []
-        listAll = []
         if not root:
-            return listAll
-        return self.Find(root, expectNumber, lis, listAll)
-
+            return []
+        if expectNumber == 0:
+            return []
+        lis = []
+        path = []
+        lis = self.Find(root, expectNumber, path, lis)
+        return lis
 
 
 pNode1 = TreeNode(10)
